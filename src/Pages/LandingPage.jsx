3b7b42087cb691de4def5424126c3c0bf85dec45
Jobsidney <jobsidney67@gmail.com>
2022-11-28 22:41:43 +0300

    partially done

--- src/Pages/LandingPage.jsx
+++ src/Pages/LandingPage.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
+import Community from '../components/Community'
 import Footer from '../components/Footer'
 import HeroSection from '../components/HeroSection'
 import NavBar from '../components/NavBar'
 
 
+
 function LandingPage() {
   return (
     <div>
-        {/* <div className='sticky '><NavBar/></div> */}
+        
         <section id='home'>
             <HeroSection/>
         </section>
@@ -16,7 +18,7 @@ function LandingPage() {
                 <h1 className='lg:w-[900px] text-3xl md:text-5xl lg:text-7xl font-extrabold tracking-wide sm:leading-10 text-black '>What We Offer</h1>
             </div>
         </section>
-
+     
         <Footer/>
 
       
--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,19 +1,20 @@
 import React from 'react'
+import NavBar from './NavBar'
 
 function HeroSection() {
   return (
     <section className="relative bg-[url(https://images.unsplash.com/photo-1604014237800-1c9102c219da?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80)] bg-cover bg-center bg-no-repeat">
-        
+        <div className='sticky float z-40'><NavBar/></div>
         <div className="absolute inset-0 bg-white/75 sm:bg-transparent sm:bg-gradient-to-r sm:from-white/60 sm:to-white/25"></div>
         
 
         <div className="relative mx-auto max-w-screen-xl px-4 py-32 sm:px-6 lg:flex lg:h-screen lg:items-center lg:px-8">
             <div className="max-w-xl text-center sm:text-left">
             <h1 className="text-3xl font-extrabold sm:text-5xl">
-                Let us find your
+                Let us find your Next 
 
                 <strong className="block font-extrabold text-rose-700">
-                Forever Home.
+                Market and Buying point
                 </strong>
             </h1>
 
