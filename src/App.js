db474b36b0f21f49d2994751fa7ecd212b441571
Jobsidney <jobsidney67@gmail.com>
2022-11-28 18:16:11 +0300

    Landing page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import LoginForm from './Pages/LoginForm';
+import SignUpForm from './Pages/SignUpForm';
+import LandingPage from './Pages/LandingPage';
 
 function App() {
   return (
     <div className="App">
 
 
-      <LoginForm/>
+      <LandingPage/>
       
     </div>
   );
--- src/Pages/SignUpForm.jsx
+++ src/Pages/SignUpForm.jsx
@@ -23,7 +23,7 @@ function SignUpForm() {
                                 <label for="password" className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Password</label>
                             </div>
                             <div className="relative">
-                                <button className="bg-blue-500 text-white rounded-md px-2 py-1">Submit</button>
+                                <button className="bg-blue-500 text-white rounded-md px-2 py-1">Register</button>
                             </div>
                         </div>
                     </div>
