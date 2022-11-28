614ebe86d0ab2872ca8668fc50632d370c8e7625
Jobsidney <jobsidney67@gmail.com>
2022-11-28 18:46:57 +0300

    Navbar

--- src/Pages/LandingPage.jsx
+++ src/Pages/LandingPage.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import NavBar from '../components/NavBar'
+
 
 function LandingPage() {
   return (
     <div>
+        {/* <div className='sticky '><NavBar/></div> */}
+        
         <section className="relative bg-[url(https://images.unsplash.com/photo-1604014237800-1c9102c219da?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80)] bg-cover bg-center bg-no-repeat">
+        
             <div className="absolute inset-0 bg-white/75 sm:bg-transparent sm:bg-gradient-to-r sm:from-white/60 sm:to-white/25"></div>
+            
 
             <div className="relative mx-auto max-w-screen-xl px-4 py-32 sm:px-6 lg:flex lg:h-screen lg:items-center lg:px-8">
                 <div className="max-w-xl text-center sm:text-left">
