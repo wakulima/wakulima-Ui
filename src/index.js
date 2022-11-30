af648ee2d1b65cd29e2317b3e8f246b53574b659
Verah Nyagaka <verahnyagaka101@gmail.com>
2022-11-30 07:21:44 +0300

    added reg code

--- src/index.js
+++ src/index.js
@@ -3,11 +3,30 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
 
+import authReducer from "./slices/authSlice";
+
+const store = configureStore({
+  reducer: {
+    
+    auth: authReducer,
+    
+  },
+  // middleware: (getDefaultMiddleware) =>
+  //   getDefaultMiddleware().concat(productsApi.middleware),
+});
+
+store.dispatch(productsFetch());
+store.dispatch(getTotals());
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
 
