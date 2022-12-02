73c6ba2cac41709061ade8c4697870cec846baf4
Jobsidney <jobsidney67@gmail.com>
2022-12-02 18:09:52 +0300

    swiper

--- src/components/ServicesSection.jsx
+++ src/components/ServicesSection.jsx
@@ -1,5 +1,14 @@
 import React from 'react'
 
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+// import required modules
+import { Autoplay, Pagination, Navigation } from "swiper";
 function ServicesSection() {
   return (
     <div>
@@ -7,7 +16,55 @@ function ServicesSection() {
             <h1 className='text-6xl font-Prata text-[#005364]'>Featured Services</h1>
             <div>
             <div className='flex flex-row justify-center space-x-16 pt-32'>
+            <Swiper
+                spaceBetween={30}
+                centeredSlides={true}
+                autoplay={{
+                    delay: 2500,
+                    disableOnInteraction: false,
+                }}
+                pagination={{
+                    clickable: true,
+                }}
+                navigation={true}
+                modules={[Autoplay, Pagination, Navigation]}
+                className="mySwiper"
+            >
+                <SwiperSlide>
                     <div className='text-center w-[400px] h-[400px]  shadow-2xl rounded-xl shadow-black/50 flex flex-col align-center items-center p-8 space-y-4'>
+                         <div className='    w-[80px] h-[80px]  bg-[#4DB852] rounded-full'></div>
+                            <h1 className='text-2xl font-bold text-[#4DB852]'>Ai enabled Supply chain</h1>
+                            <p className='text-lg'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.</p>
+
+                     </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className='hover:animate-pulse p-8 text-center space-y-4 w-[400px] h-[400px] bg-[#4DB852] bg-gradient-to-r  shadow-2xl rounded-xl shadow-black/50 flex flex-col align-center items-center '>
+                        <div className='    w-[80px] h-[80px]  bg-[white] rounded-full '></div>
+                        <h1 className='text-2xl font-bold text-[white]'>Market Analysis</h1>
+                            <p className='text-lg text-white'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.</p>
+
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className=' text-center w-[400px] h-[400px]  shadow-2xl rounded-xl shadow-black/50 flex flex-col align-center items-center p-8 space-y-4'>
+                            <div className='    w-[80px] h-[80px]  bg-[#4DB852] rounded-full'></div>
+                            <h1 className='text-2xl font-bold text-[#4DB852]'>Farmer Connection</h1>
+                            <p className='text-lg'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.</p>
+
+                    </div>
+                </SwiperSlide>
+                </Swiper>
+
+
+
+            <div className='text-center w-[400px] h-[400px]  shadow-2xl rounded-xl shadow-black/50 flex flex-col align-center items-center p-8 space-y-4'>
+                <div className='    w-[80px] h-[80px]  bg-[#4DB852] rounded-full'></div>
+                    <h1 className='text-2xl font-bold text-[#4DB852]'>Ai enabled Supply chain</h1>
+                    <p className='text-lg'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.</p>
+
+            </div>
+                    {/* <div className='text-center w-[400px] h-[400px]  shadow-2xl rounded-xl shadow-black/50 flex flex-col align-center items-center p-8 space-y-4'>
                         <div className='    w-[80px] h-[80px]  bg-[#4DB852] rounded-full'></div>
                         <h1 className='text-2xl font-bold text-[#4DB852]'>Ai enabled Supply chain</h1>
                         <p className='text-lg'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.</p>
@@ -28,7 +85,7 @@ function ServicesSection() {
                         <h1 className='text-2xl font-bold text-[#4DB852]'>Farmer Connection</h1>
                         <p className='text-lg'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.</p>
 
-                    </div>
+                    </div> */}
                 </div>
             </div>
                     
