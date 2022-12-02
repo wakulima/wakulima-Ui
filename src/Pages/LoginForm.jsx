9131fb4bda67789158e85e20cdb93f9f7fd65b5e
Verah Nyagaka <verahnyagaka101@gmail.com>
2022-12-02 08:21:24 +0300

    updated code

--- src/Pages/LoginForm.jsx
+++ src/Pages/LoginForm.jsx
@@ -33,7 +33,7 @@ function LoginForm() {
     <form onSubmit={handleSubmit}>
     <div className="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
       <div className="relative py-3 sm:max-w-xl sm:mx-auto">
-        <div className="absolute inset-0 bg-gradient-to-r from-blue-300 to-blue-600 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
+        <div className="absolute inset-0 bg-gradient-to-r from-green-300 to-green-600 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
         <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
           <div className="max-w-md mx-auto">
             <div>
@@ -76,7 +76,7 @@ function LoginForm() {
                   </label>
                 </div>
                 <div className="relative">
-                  <button className="bg-blue-500 text-white rounded-md px-2 py-1">
+                  <button className="bg-green-500 text-white rounded-md px-2 py-1">
                   {auth.loginStatus === "pending" ? "Submitting..." : "Login"}
                   </button>
                 </div>
