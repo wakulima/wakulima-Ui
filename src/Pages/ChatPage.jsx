106c9950b6726ee64629747f18f9d861f5904582
Jobsidney <jobsidney67@gmail.com>
2022-12-09 09:15:36 +0300

    chat section refactor

--- src/Pages/ChatPage.jsx
+++ src/Pages/ChatPage.jsx
@@ -7,7 +7,7 @@ function ChatPage() {
   return (
     <div className='font-Poppins'>
       
-      <div className="h-screen container mx-auto shadow-lg rounded-lg">
+      <div className="h-screen container shadow-black/50 mx-auto shadow-2xl rounded-lg">
     
     <div className="px-5 py-5 flex justify-between items-center bg-white border-b-2">
       <div className="font-semibold text-2xl">Farmers chat</div>
