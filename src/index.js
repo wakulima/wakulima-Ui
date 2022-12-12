6368758fc98b05d7e3abfbe650b91425a41faa60
Verah Nyagaka <verahnyagaka101@gmail.com>
2022-12-12 14:47:23 +0300

    updated

--- src/index.js
+++ src/index.js
@@ -9,6 +9,8 @@ import { configureStore } from "@reduxjs/toolkit";
 import authReducer from './components/slices/Authslice';
 import productsReducer from './components/products/slices/productSlice';
 import { productsApi } from './components/products/slices/productApi';
+import { researchpostApi } from './components/products/slices/rpostApi';
+import research_infosReducer  from './components/products/slices/rpostSlice';
 import cartReducer from './components/products/slices/cartSlice';
 // import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -18,11 +20,16 @@ const store = configureStore({
     auth: authReducer,
     products: productsReducer,
     cart: cartReducer,
+    research_infos: research_infosReducer,
+ 
     [productsApi.reducerPath]: productsApi.reducer,
+    [researchpostApi.reducerPath]: researchpostApi.reducer,
     
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(productsApi.middleware),
+    (getDefaultMiddleware().concat(productsApi.middleware),
+    getDefaultMiddleware().concat(researchpostApi.middleware)
+    ),
 });
 
 // store.dispatch(productsFetch());
