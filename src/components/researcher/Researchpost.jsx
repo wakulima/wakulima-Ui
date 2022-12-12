77fa37eeeaea6a17d29e0a91338b91e732a9747a
Verah Nyagaka <verahnyagaka101@gmail.com>
2022-12-12 15:09:50 +0300

    updated

--- src/components/researcher/Researchpost.jsx
+++ src/components/researcher/Researchpost.jsx
@@ -24,10 +24,9 @@ function Researchpost() {
     <Card style={{ width: '18rem', height:'16rem' }}>
       <Card.Img style={{ height: '16rem' }} variant="top" src={product.picture} />
       <Card.Body>
-        <Card.Title>Card Title</Card.Title>
+        <Card.Title>{product.title}</Card.Title>
         <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
+          {product.content}
         </Card.Text>
         <Button variant="primary">Go somewhere</Button>
       </Card.Body>
